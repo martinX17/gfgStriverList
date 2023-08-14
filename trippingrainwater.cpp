7e5f6d6f9e439d25d0631657de5a1255578dc408
martinX17 <yakubmaniyar1709@gmail.com>
2023-08-14 07:31:50 +0530

    Edited

--- trippingrainwater.cpp
+++ trippingrainwater.cpp
@@ -1,3 +1,10 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
 class Solution{
 
     // Function to find the trapped water between the blocks.
@@ -26,3 +33,33 @@ class Solution{
         return ans;
     }
 };
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
